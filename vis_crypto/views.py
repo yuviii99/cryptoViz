92619bbe62fd2976194772fdcd21be5f793cf93d
Yuvraj Singh Rana <ranayuvraj99@gmail.com>
2024-08-16 05:26:33 +1000

    Update index method

--- vis_crypto/views.py
+++ vis_crypto/views.py
@@ -4,7 +4,20 @@ import requests
 from datetime import datetime
 
 def index(request):
-    return render(request, "vis_crypto/base.html")
+    coin_market_chart_data = fetch_coin_market_chart()
+    date_time_strings = extract_price_data(coin_market_chart_data)
+    actual_price = [lst[1] for lst in coin_market_chart_data["prices"]]
+
+    return render(request, "Vis_Cryp/base.html", {
+        'content1': {
+            'template_name': 'Vis_Cryp/chart.html',
+            'data': {
+                'crypData': date_time_strings[0:100],
+                'actualPrice': actual_price[0:100],
+                'currency': 'usd',
+            },
+        }
+    })
 
 def fetch_coin_market_chart(crypto_coin='bitcoin', versus_currency='usd'):
     query_params = {
