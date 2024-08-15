26e8d4281d4745e62ee84eac906fc0d3814ba731
Yuvraj Singh Rana <ranayuvraj99@gmail.com>
2024-08-16 05:25:42 +1000

    Add extract price method

--- vis_crypto/views.py
+++ vis_crypto/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 import os
 import requests
+from datetime import datetime
 
 def index(request):
     return render(request, "vis_crypto/base.html")
@@ -18,3 +19,9 @@ def fetch_coin_market_chart(crypto_coin='bitcoin', versus_currency='usd'):
     api_endpoint = f'https://api.coingecko.com/api/v3/coins/{crypto_coin}/market_chart'
     response = requests.get(api_endpoint, params=query_params, headers=headers).json()
     return response
+
+def extract_price_data(data):
+    timestamps = [timestamp / 1000 for timestamp, _ in data["prices"]]
+    date_times = [datetime.fromtimestamp(ts) for ts in timestamps]
+    date_time_strings = [dt.strftime('%A %d %B') for dt in date_times]
+    return date_time_strings
