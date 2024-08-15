a7584ca104637120890437d93afef13bbed5d9e3
Yuvraj Singh Rana <ranayuvraj99@gmail.com>
2024-08-16 05:43:15 +1000

    fix index method

--- vis_crypto/views.py
+++ vis_crypto/views.py
@@ -8,9 +8,9 @@ def index(request):
     date_time_strings = extract_price_data(coin_market_chart_data)
     actual_price = [lst[1] for lst in coin_market_chart_data["prices"]]
 
-    return render(request, "Vis_Cryp/base.html", {
+    return render(request, "vis_crypto/base.html", {
         'content1': {
-            'template_name': 'Vis_Cryp/chart.html',
+            'template_name': 'vis_crypto/chart.html',
             'data': {
                 'crypData': date_time_strings[0:100],
                 'actualPrice': actual_price[0:100],
