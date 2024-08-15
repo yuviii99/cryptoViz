c160a6868bdf2d1a18f3850e65005ea9104c4dc0
Yuvraj Singh Rana <ranayuvraj99@gmail.com>
2024-08-16 06:30:58 +1000

    handle post req //TODO: csrf_fix

--- vis_crypto/views.py
+++ vis_crypto/views.py
@@ -2,32 +2,50 @@ from django.shortcuts import render
 import os
 import requests
 from datetime import datetime
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
+# TODO: Fix csrf token
+@csrf_exempt
 def index(request):
-    coin_market_chart_data = fetch_coin_market_chart()
-    date_time_strings = extract_price_data(coin_market_chart_data)
-    actual_price = [lst[1] for lst in coin_market_chart_data["prices"]]
-    
-    coin_list_data = fetch_coin_list()
-    coin_ids = extract_coin_ids(coin_list_data)
+    if request.method == 'POST':
+        versus_currency = request.POST.get('selected_currency')
+        crypto_coin = request.POST.get('selected_option')
 
-    currency_symbols = [
-        'usd', 'eur', 'jpy', 'gbp', 'aud', 'cad', 'chf', 'cny', 'sek', 'nzd',
-        'mxn', 'sgd', 'hkd', 'nok', 'krw', 'inr', 'rub', 'brl', 'zar', 'try'
-    ]
+        coin_market_chart_data = fetch_coin_market_chart(crypto_coin,versus_currency)
+        date_time_strings = extract_price_data(coin_market_chart_data)
+        actual_price = [lst[1] for lst in coin_market_chart_data["prices"]]
 
-    return render(request, "vis_crypto/base.html", {
-        'content1': {
-            'template_name': 'vis_crypto/chart.html',
-            'data': {
-                'crypData': date_time_strings[0:100],
-                'actualPrice': actual_price[0:100],
-                'currency': 'usd',
-                'coin_id_list': coin_ids,
-                'currency_symbols': currency_symbols
-            },
-        }
-    })
+        return JsonResponse({
+            'crypData': date_time_strings[0:100],
+            'actualPrice': actual_price[0:100],
+            'currency': versus_currency,
+        })
+    else:
+        coin_market_chart_data = fetch_coin_market_chart()
+        date_time_strings = extract_price_data(coin_market_chart_data)
+        actual_price = [lst[1] for lst in coin_market_chart_data["prices"]]
+
+        coin_list_data = fetch_coin_list()
+        coin_ids = extract_coin_ids(coin_list_data)
+
+        currency_symbols = [
+            'usd', 'eur', 'jpy', 'gbp', 'aud', 'cad', 'chf', 'cny', 'sek', 'nzd',
+            'mxn', 'sgd', 'hkd', 'nok', 'krw', 'inr', 'rub', 'brl', 'zar', 'try'
+        ]
+
+        return render(request, "vis_crypto/base.html", {
+            'content1': {
+                'template_name': 'vis_crypto/chart.html',
+                'data': {
+                    'crypData': date_time_strings[0:100],
+                    'actualPrice': actual_price[0:100],
+                    'currency': 'usd',
+                    'coin_id_list': coin_ids,
+                    'currency_symbols': currency_symbols
+                },
+            }
+        })
 
 def fetch_coin_market_chart(crypto_coin='bitcoin', versus_currency='usd'):
     query_params = {
